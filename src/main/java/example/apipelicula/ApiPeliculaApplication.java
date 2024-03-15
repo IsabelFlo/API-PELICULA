99c9134ed0ed21c18462c6781b1151286eb2765a
MCVdev <mateocarvajalvergara333@gmail.com>
2024-03-14 22:10:05 -0500

    first commit

---
+++ src/main/java/example/apipelicula/ApiPeliculaApplication.java
@@ -0,0 +1,13 @@
+package example.apipelicula;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiPeliculaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiPeliculaApplication.class, args);
+	}
+
+}
